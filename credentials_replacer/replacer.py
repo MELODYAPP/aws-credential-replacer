d1fd12c3a5dfc49b29f735ade00060477be71742
Jocelyn Boullier <kazoomy@gmail.com>
2017-07-31 19:12:27 +0200

    Add support for region and encryption context

--- credentials_replacer/replacer.py
+++ credentials_replacer/replacer.py
@@ -7,7 +7,7 @@ from credstash import getSecret, listSecrets
 from jinja2 import Environment, FileSystemLoader
 
 
-def render_with_credentials(file):
+def render_with_credentials(file, region, context):
     """Render file argument with credstash credentials
 
     Load file as jinja2 template and render it with context where keys are 
@@ -22,21 +22,35 @@ def render_with_credentials(file):
     """
     env = Environment(loader=FileSystemLoader(os.path.dirname(file)))
     template = env.get_template(os.path.basename(file))
-    context = {secret['name']: getSecret(secret['name'])
-               for secret in listSecrets()}
-    return template.render(**context)
+
+    secrets = {
+        secret['name']: getSecret(
+            secret['name'], region=region, context=context
+        ) for secret in listSecrets(region=region)}
+    return template.render(**secrets)
+
+
+def parse_key_value(ctx, param, value):
+    d = {
+        key: value for key, value 
+        in map(lambda s: s.split('='), value)
+    }
+    return d
 
 
 @click.command()
-@click.argument('file')
-def main(file):
+@click.option('-r', default='us-east-1', help='DynamoDB region')
+@click.argument('file', nargs=1)
+@click.argument('context', nargs=-1, callback=parse_key_value, 
+                help='Encryption context key/value pairs to pass as `key=value`')
+def main(r, file, context=None):
     """Output rendered template
 
     Args:
         file (str): jinja2 template file path
 
     """
-    sys.stdout.write(render_with_credentials(file))
+    sys.stdout.write(render_with_credentials(file, r, context))
 
 
 if __name__ == '__main__':
