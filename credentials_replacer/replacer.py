2d75f7c78a9b821d7f118adeef9a4f6a18938b56
Jocelyn Boullier <kazoomy@gmail.com>
2017-08-01 13:16:59 +0200

    Add strict option to raise error on undefined variable

--- credentials_replacer/replacer.py
+++ credentials_replacer/replacer.py
@@ -4,10 +4,10 @@ import sys
 
 import click
 from credstash import getSecret, listSecrets
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
 
 
-def render_with_credentials(file, region, context):
+def render_with_credentials(file, region, context, strict=False):
     """Render file argument with credstash credentials
 
     Load file as jinja2 template and render it with context where keys are 
@@ -20,7 +20,14 @@ def render_with_credentials(file, region, context):
         str: Rendered string
 
     """
-    env = Environment(loader=FileSystemLoader(os.path.dirname(file)))
+    env_args = {
+        'loader': FileSystemLoader(os.path.dirname(file)),
+    }
+
+    if strict:
+        env_args['undefined'] = StrictUndefined
+
+    env = Environment(**env_args)
     template = env.get_template(os.path.basename(file))
 
     secrets = {
@@ -40,16 +47,22 @@ def parse_key_value(ctx, param, value):
 
 @click.command()
 @click.option('-r', default='us-east-1', help='DynamoDB region')
+@click.option(
+    '--strict', 
+    is_flag=True,
+    default=False, 
+    help="Raise an exception if a template variable doesn't exist"
+)
 @click.argument('file', nargs=1)
 @click.argument('context', nargs=-1, callback=parse_key_value)
-def main(r, file, context=None):
+def main(r, strict, file, context=None):
     """Output rendered template
 
     Args:
         file (str): jinja2 template file path
 
     """
-    sys.stdout.write(render_with_credentials(file, r, context))
+    sys.stdout.write(render_with_credentials(file, r, context, strict))
 
 
 if __name__ == '__main__':
